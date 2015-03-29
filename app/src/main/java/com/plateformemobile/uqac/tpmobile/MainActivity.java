7f1ec9b39db1e46715e4524256752b3d07244cc0
Steven GERARD <steven.gerard.deruelle@gmail.com>
2015-03-29 18:31:48 -0400

    Ajout menu principale

--- app/src/main/java/com/plateformemobile/uqac/tpmobile/MainActivity.java
+++ app/src/main/java/com/plateformemobile/uqac/tpmobile/MainActivity.java
@@ -1,17 +1,68 @@
 package com.plateformemobile.uqac.tpmobile;
 
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    static final String CAMERA_PIC_DIR = "/DCIM/Text/";
+    File noteDisplay;
+    private List<String> directoryEntries = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textDisplay = (TextView)findViewById(R.id.noteText);
+
+        textDisplay.append("toto");
+
+        String path = "/storage/sdcard0" + CAMERA_PIC_DIR;
+
+        File directory = new File(path);
+        textDisplay.append(Environment.getExternalStorageDirectory().getAbsolutePath());
+        if (directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            //sort in descending date order
+            Arrays.sort(files, new Comparator<File>() {
+                public int compare(File f1, File f2) {
+                    return -Long.valueOf(f1.lastModified())
+                            .compareTo(f2.lastModified());
+                }
+            });
+
+            this.directoryEntries.clear();
+            for (File file : files) {
+                this.directoryEntries.add(file.getName());
+            }
+
+            ArrayAdapter<String> directoryList
+                    = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, this.directoryEntries);
+            //alphabetize entries
+            //directoryList.sort(null);
+
+            ListView list = (ListView)findViewById(R.id.noteList);
+            list.setAdapter(directoryList);
+        }
+        else {
+            textDisplay.append("toto3");
+        }
     }
 
 
